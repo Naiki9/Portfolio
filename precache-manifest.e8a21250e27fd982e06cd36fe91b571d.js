117ebfd766833db08e9f86ab2fef38a7a351ae78
Naiki9 <olabisi94@gmail.com>
2019-09-20 19:07:01 +0100

    Updates

---
+++ precache-manifest.e8a21250e27fd982e06cd36fe91b571d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba995e412a684096641fadfc011a970",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d28e11cc96a66da5613",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "15b079182fe0e86e96c1",
+    "url": "/portfolio/static/js/2.877e2868.chunk.js"
+  },
+  {
+    "revision": "0d28e11cc96a66da5613",
+    "url": "/portfolio/static/js/main.e32153d9.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/portfolio/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "1651ac0d07bb9054124d93b16971d341",
+    "url": "/portfolio/static/media/css.1651ac0d.png"
+  },
+  {
+    "revision": "1706c9f16bd08eb5e03f1df3e0a94a1c",
+    "url": "/portfolio/static/media/figma.1706c9f1.png"
+  },
+  {
+    "revision": "a66b9be19ec11b5cc58304f60e924fcc",
+    "url": "/portfolio/static/media/header-bg.a66b9be1.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/portfolio/static/media/header-bgc.e2d2a51c.jpg"
+  },
+  {
+    "revision": "ca522180f25dc88e135c54a1d43c78fd",
+    "url": "/portfolio/static/media/ionic.ca522180.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/portfolio/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "e068a7fe43fbf17c16de64f26c5e7ccc",
+    "url": "/portfolio/static/media/javascript.e068a7fe.png"
+  },
+  {
+    "revision": "076aee3cc79294eca8643a1c05a42e4d",
+    "url": "/portfolio/static/media/mdb.076aee3c.png"
+  },
+  {
+    "revision": "6d390c1e894a80125b2b0ae77d11ae97",
+    "url": "/portfolio/static/media/php.6d390c1e.png"
+  },
+  {
+    "revision": "88b5e0ec0c5bdd59e7a1362add84bb57",
+    "url": "/portfolio/static/media/react.88b5e0ec.png"
+  },
+  {
+    "revision": "b01ac5a00a024c39f9a1be74589cbfd9",
+    "url": "/portfolio/static/media/uiux.b01ac5a0.jpeg"
+  },
+  {
+    "revision": "bb85524f60a0974f372f9d01a1c31ce7",
+    "url": "/portfolio/static/media/webdesign.bb85524f.jpeg"
+  },
+  {
+    "revision": "7569ea2e31240b425a74dc11f83c6bdf",
+    "url": "/portfolio/static/media/webdev.7569ea2e.jpeg"
+  }
+]);
